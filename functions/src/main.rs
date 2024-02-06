6f0654b948faa1dccf5403984e54c6f0ad36521f
Daniel Lucas <hi@danlgz.io>
2024-02-05 21:31:58 -0600

    chore: returning comment

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -9,11 +9,11 @@ fn main() {
     let num = {
         // run this scope block expressin, return the result and assign it to `num`
         let x = 3;
-        x * 5
+        x * 5 // no semicolon indicates that the value is return it
     };
     println!("num {num}");
 }
 
 fn duplicate_value(val: i32) -> i32 {
-    val * 2
+    val * 2 // no semicolon indicates that the value is return it
 }
